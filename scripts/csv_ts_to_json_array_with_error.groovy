7b5f1caa1c929566fe661cd404331e91a4f1a51c
gihankarunarathne <gckarunarathne@gmail.com>
2019-03-08 19:38:15 +0400

    Added groovy script to create timeseries for input with error data for extensions

---
+++ scripts/csv_ts_to_json_array_with_error.groovy
@@ -0,0 +1,41 @@
+import groovy.json.JsonBuilder
+
+def reqSizeMap = [
+        24  : 'ts_1hr',
+        288 : 'ts_5min',
+        1440: 'ts_1min'
+]
+
+class Point {
+    String time;
+    float value
+}
+
+List<Point> pointList = new ArrayList<>()
+
+def reqSize = vars.get("reqSize") as Integer
+//log.info("Req Size:" + reqSizeMap.get(reqSize))
+def file = new File("./data/${reqSizeMap.get(reqSize)}.csv")
+file.eachLine { line, number ->
+    if (number == 1)
+        return
+    String[] str = line.split(',')
+    if (str.length != 2)
+        return
+    pointList.add(new Point(time: str[0].trim(), value: Float.parseFloat(str[1].trim())))
+}
+
+def jsonBuilder = new JsonBuilder()
+jsonBuilder(
+        pointList.collect { point ->
+            [
+                    time : point.time,
+                    value: point.value
+            ]
+        }
+)
+
+// log.info("Message:" + vars.get(jsonBuilder.toPrettyString()))
+
+sampler.addNonEncodedArgument("", jsonBuilder.toPrettyString(), "")
+sampler.setPostBodyRaw(true)
