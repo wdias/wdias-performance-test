27112d7d12d02605974f11aa728eb0cd34e9ad85
gihankarunarathne <gckarunarathne@gmail.com>
2019-03-08 12:01:48 +0400

    Implemented Creating Extension create request body

---
+++ scripts/csv_to_extension_transformation_json.groovy
@@ -0,0 +1,86 @@
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+class Parameter {
+    String parameterId
+    String variable
+    String unit
+    String parameterType
+}
+
+class Location {
+    String locationId
+    String name
+    String lat
+    String lon
+}
+
+class TimeStep {
+    String timeStepId
+    String unit
+    String multiplier
+}
+
+class Variable {
+    String variableId
+    String moduleId
+    String valueType
+    Parameter parameter
+    Location location
+    String timeseriesType
+    TimeStep timeStep
+}
+
+def jsonSlurper = new JsonSlurper()
+List<Variable> variablesList = [new Variable(
+        variableId: "Input",
+        moduleId: vars.get("moduleId").trim(),
+        valueType: vars.get("valueType").trim(),
+        parameter: jsonSlurper.parseText(vars.get("parameter").trim()),
+        location: jsonSlurper.parseText(vars.get("location").trim()),
+        timeseriesType: vars.get("timeseriesType").trim(),
+        timeStep: jsonSlurper.parseText(vars.get("timeStep").trim())
+), new Variable(
+        variableId: "Output",
+        moduleId: vars.get("moduleId").trim(),
+        valueType: vars.get("valueType").trim(),
+        parameter: jsonSlurper.parseText(vars.get("parameter").trim()),
+        location: jsonSlurper.parseText(vars.get("location").trim()),
+        timeseriesType: vars.get("timeseriesType").trim(),
+        timeStep: jsonSlurper.parseText(vars.get("timeStep").trim())
+)]
+
+def jsonBuilder = new JsonBuilder()
+jsonBuilder {
+    extensionId "transformation_aggregate_accumulative"
+    extension "Transformation"
+    function "AggregateAccumulative"
+    variables variablesList.collect { Variable v ->
+        [
+                variableId: v.variableId,
+                metadata  : {
+                    moduleId v.moduleId
+                    valueType v.valueType
+                    parameter v.parameter
+                    location v.location
+                    timeseriesType v.timeseriesType
+                    timeStep v.timeStep
+                }
+        ]
+    }
+    inputVariables jsonSlurper.parseText("[\"Input\"]")
+    outputVariables jsonSlurper.parseText("[\"Output\"]")
+    trigger jsonSlurper.parseText('''
+        [{
+            "trigger_type": "OnChange",
+            "trigger_on": ["Input"]
+        }]
+     ''')
+    options {
+        ignoringMissing "True"
+    }
+}
+log.info("Message:" + vars.get(jsonBuilder.toPrettyString()))
+
+sampler.addNonEncodedArgument("", jsonBuilder.toPrettyString(), "")
+sampler.setPostBodyRaw(true)
