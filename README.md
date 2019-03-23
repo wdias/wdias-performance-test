07ee63420d56155c6ea5cafae1b15e8585b0b970
gihankarunarathne <gckarunarathne@gmail.com>
2019-03-23 17:10:08 +0400

    Integrated status-service for waiting for extension timeseries to be completed

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # wdias-performance-test
 Distributed performance testing based on JMeter.
 
-### Run via Command Line
+## Run via Command Line
 ```
 ./apache-jmeter-5.0/bin/jmeter.sh -n -t wdias_performance_test.jmx -l testresults.jtl -j non-ui.log -JreqSize=24
 ```
 - https://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
 - Variable can be provide as properties file - https://gerardnico.com/jmeter/property_file#userproperties
+
+## Dev Guide
+#### How to run grrovy code
+- Download Groovy same as JMeter JSR223 version and extract into the folder (Add permission to run, if required)
+- `./groovy-2.4.16/bin/groovy ./scripts/wait_for_extension_timeseries.groovy`
---
+++ scripts/wait_for_extension_timeseries.groovy
@@ -0,0 +1,34 @@
+def nullTrustManager = [
+    checkClientTrusted: { chain, authType ->  },
+    checkServerTrusted: { chain, authType ->  },
+    getAcceptedIssuers: { null }
+]
+
+def nullHostnameVerifier = [
+    verify: { hostname, session -> 
+        //true 
+        hostname.endsWith('noondv.com')
+    }
+]
+
+javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext.getInstance("SSL")
+sc.init(null, [nullTrustManager as  javax.net.ssl.X509TrustManager] as  javax.net.ssl.X509TrustManager[], null)
+javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
+javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(nullHostnameVerifier as javax.net.ssl.HostnameVerifier)
+// -- Disable SSL: https://gist.github.com/barata0/63705c0bcdd1054af2405e90c06f6b71
+
+def DOMAIN = "noondv.com"
+//def DOMAIN = vars.get("DOMAIN") as String
+def requestId = "ScalarRequest"
+//def requestId =  vars.get("requestId").trim()
+
+SampleResult.sampleStart()
+def url = new URL("https://api.${DOMAIN}/status/import/scalar/${requestId}")
+while ({
+     sleep(1000)
+     def get = url.openConnection()
+     def getRC = get.getResponseCode()
+     // log.info("${getRC}")
+     getRC == 400 // breaks if status code is 200
+}()) continue // https://stackoverflow.com/a/22057667
+SampleResult.sampleEnd()
