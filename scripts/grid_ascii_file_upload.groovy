49a8707bdc01851763db7af21a8d89a47cf817c0
gihankarunarathne <gckarunarathne@gmail.com>
2019-03-24 14:20:13 +0400

    Implemented uploading ascii grid files via groovy script
    - https://www.blazemeter.com/blog/top-8-jmeter-java-classes-you-should-be-using-with-groovy

---
+++ scripts/grid_ascii_file_upload.groovy
@@ -0,0 +1,34 @@
+import org.apache.jmeter.protocol.http.util.HTTPFileArgs
+
+def reqSizeMap = [
+        24  : 60,
+        288 : 30,
+        1440: 15
+]
+def dateMap = [
+        24  : '2017-05-24',
+        288 : '2017-05-25',
+        1440: '2017-05-26'
+]
+
+def reqSize = vars.get("reqSize") as Integer
+
+def noHr = 24
+def gap = reqSizeMap.get(reqSize)
+def noMin = 60 / gap
+
+HTTPFileArgs filesToSend = new HTTPFileArgs()
+
+String dateStr = dateMap.get(reqSize)
+noHr.times {
+    def hour = it
+    noMin.times {
+        def path = "./water_level_grid/${gap}_min/${dateStr}_${String.format("%02d", hour)}-${String.format("%02d", it*gap)}-00.asc"
+        def paramName = "${dateStr}T${String.format("%02d", hour)}:${String.format("%02d", it*gap)}:00Z"
+        filesToSend.addHTTPFileArg(path, paramName, "text/plain")
+    }
+}
+// https://jmeter.apache.org/api/org/apache/jmeter/protocol/http/sampler/HTTPSamplerBase.html#setHTTPFiles-org.apache.jmeter.protocol.http.util.HTTPFileArg:A-
+// 4. HTTPSamplerProxy Class - https://www.blazemeter.com/blog/top-8-jmeter-java-classes-you-should-be-using-with-groovy
+sampler.setHTTPFiles(filesToSend.asArray())
+sampler.setDoMultipart(true)
