4335ae9649cca757e0cdc7dc24dbbc0cd39091c0
gihankarunarathne <gckarunarathne@gmail.com>
2019-05-09 17:23:53 +0400

    Improved script to create and get timeseries based on DataType & reqSize

--- scripts/csv_to_timeseries_metadata_json.groovy
+++ scripts/csv_to_timeseries_metadata_json.groovy
@@ -2,15 +2,48 @@ import org.apache.jmeter.protocol.http.control.Header
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 
+def timeStepMap = [
+        24  : "{\"timeStepId\":\"each_hour\",\"unit\":\"Hour\",\"multiplier\":1}",
+        288 : "{\"timeStepId\":\"each_5_min\",\"unit\":\"Minute\",\"multiplier\":5}",
+        1440: "{\"timeStepId\":\"each_min\",\"unit\":\"Minute\",\"multiplier\":1}"
+]
+def gridTimeStepMap = [
+        24  : "{\"timeStepId\":\"each_60_min\",\"unit\":\"Minute\",\"multiplier\":60}",
+        288 : "{\"timeStepId\":\"each_30_min\",\"unit\":\"Minute\",\"multiplier\":30}",
+        1440: "{\"timeStepId\":\"each_15_min\",\"unit\":\"Minute\",\"multiplier\":15}"
+]
+int reqSize = vars.get("reqSize") as Integer
+def DataType = vars.get("DataType")
+
 def jsonSlurper = new JsonSlurper()
 def jsonBuilder = new JsonBuilder()
-jsonBuilder {
-    moduleId vars.get("moduleId").trim()
-    valueType vars.get("valueType").trim()
-    parameter jsonSlurper.parseText(vars.get("parameter").trim())
-    location jsonSlurper.parseText(vars.get("location").trim())
-    timeseriesType vars.get("timeseriesType").trim()
-    timeStep jsonSlurper.parseText(vars.get("timeStep").trim())
+if (DataType == "Grid") {
+    jsonBuilder {
+        moduleId vars.get("gModuleId").trim()
+        valueType vars.get("gValueType").trim()
+        parameter jsonSlurper.parseText(vars.get("gParameter").trim())
+        location jsonSlurper.parseText(vars.get("gLocation").trim())
+        timeseriesType vars.get("gTimeseriesType").trim()
+        timeStep jsonSlurper.parseText(gridTimeStepMap.get(reqSize))
+    }
+} else if (DataType == "Vector") {
+    jsonBuilder {
+        moduleId vars.get("moduleId").trim()
+        valueType "Vector"
+        parameter jsonSlurper.parseText(vars.get("parameter").trim())
+        location jsonSlurper.parseText(vars.get("location").trim())
+        timeseriesType vars.get("timeseriesType").trim()
+        timeStep jsonSlurper.parseText(timeStepMap.get(reqSize))
+    }
+} else {
+    jsonBuilder {
+        moduleId vars.get("moduleId").trim()
+        valueType vars.get("valueType").trim()
+        parameter jsonSlurper.parseText(vars.get("parameter").trim())
+        location jsonSlurper.parseText(vars.get("location").trim())
+        timeseriesType vars.get("timeseriesType").trim()
+        timeStep jsonSlurper.parseText(timeStepMap.get(reqSize))
+    }
 }
 // log.info("Message:" + jsonBuilder.toPrettyString());
 
--- scripts/csv_to_timeseries_query_string.groovy
+++ scripts/csv_to_timeseries_query_string.groovy
@@ -1,15 +1,39 @@
 import groovy.json.JsonSlurper
 
+def timeStepMap = [
+        24  : "{\"timeStepId\":\"each_hour\",\"unit\":\"Hour\",\"multiplier\":1}",
+        288 : "{\"timeStepId\":\"each_5_min\",\"unit\":\"Minute\",\"multiplier\":5}",
+        1440: "{\"timeStepId\":\"each_min\",\"unit\":\"Minute\",\"multiplier\":1}"
+]
+def gridTimeStepMap = [
+        24  : "{\"timeStepId\":\"each_60_min\",\"unit\":\"Minute\",\"multiplier\":60}",
+        288 : "{\"timeStepId\":\"each_30_min\",\"unit\":\"Minute\",\"multiplier\":30}",
+        1440: "{\"timeStepId\":\"each_15_min\",\"unit\":\"Minute\",\"multiplier\":15}"
+]
+int reqSize = vars.get("reqSize") as Integer
+def DataType = vars.get("DataType")
+
 def jsonSlurper = new JsonSlurper()
 
 def params = [
         moduleId      : vars.get("moduleId").trim(),
-        valueType     : vars.get("valueType").trim(),
+        valueType     : DataType,
         parameterId   : jsonSlurper.parseText(vars.get("parameter").trim()).parameterId,
         locationId    : jsonSlurper.parseText(vars.get("location").trim()).locationId,
         timeseriesType: vars.get("timeseriesType").trim(),
-        timeStepId    : jsonSlurper.parseText(vars.get("timeStep").trim()).timeStepId
+        timeStepId    : jsonSlurper.parseText(timeStepMap.get(reqSize)).timeStepId
 ]
+if (DataType == 'Grid') {
+    params = [
+            moduleId      : vars.get("gModuleId").trim(),
+            valueType     : vars.get("gValueType").trim(),
+            parameterId   : jsonSlurper.parseText(vars.get("gParameter").trim()).parameterId,
+            locationId    : jsonSlurper.parseText(vars.get("gLocation").trim()).locationId,
+            timeseriesType: vars.get("gTimeseriesType").trim(),
+            timeStepId    : jsonSlurper.parseText(gridTimeStepMap.get(reqSize)).timeStepId
+    ]
+}
+
 def query_string = params.collect { k, v -> "$k=$v" }.join('&')
 
 vars.put("query_string", query_string)
