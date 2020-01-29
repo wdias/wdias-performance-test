7499a59451fd37899ac53282b5c404bae347f04c
gihankarunarathne <gckarunarathne@gmail.com>
2020-01-29 22:28:18 +0400

    changed init for all dev

--- test-plan/misc_test.sh
+++ test-plan/misc_test.sh
@@ -5,8 +5,8 @@ export MASTER_NAME=$(kubectl get pods -l wdias=jmeter-master -o jsonpath='{.item
 
 # Setup test cases
 misc_setup() {
-  export MASTER_NAME=$(kubectl get pods -l wdias=jmeter-master -o jsonpath='{.items[*].metadata.name}') && \
-  kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter setup 1440" && \
+  export MASTER_NAME=$(kubectl get pods -l wdias=jmeter-master -o jsonpath='{.items[*].metadata.name}')
+  kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter setup 1440" && sleep 3 && \
   kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter create_timeseries 1440"
 }
 
@@ -15,7 +15,9 @@ misc_run() {
   REQ_SIZE=$2
   echo "Running test: ${TEST_CASE} for reqSize: ${REQ_SIZE}"
   kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter ${TEST_CASE} ${REQ_SIZE}"
+  echo "Copy jmeter output wdias_${TEST_CASE}.jtl >> wdias_${TEST_CASE}_${REQ_SIZE}.jtl"
   kubectl get pods | grep 'wdias-performance-test-master' | awk '{print $1}' | xargs -o -I {} kubectl cp default/{}:/jmeter/logs/wdias_${TEST_CASE}.jtl ./logs/wdias_${TEST_CASE}_${REQ_SIZE}.jtl
+  echo "Copy SQLite database wdias.db >> wdias_${TEST_CASE}_${REQ_SIZE}.db"
   kubectl get pods | grep 'wdias-data-collector' | awk '{print $1}' | xargs -o -I {} kubectl cp default/{}:/go/src/app/wdias.db ./db/wdias_${TEST_CASE}_${REQ_SIZE}.db
 }
 
