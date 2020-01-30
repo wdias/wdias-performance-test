debeeac61d6ad5df03abaa1ade624f18ae28e744
gihankarunarathne <gckarunarathne@gmail.com>
2020-01-31 01:50:22 +0400

    ..

--- test-plan/misc_test.sh
+++ test-plan/misc_test.sh
@@ -10,9 +10,10 @@ export MASTER_NAME=$(kubectl get pods -l wdias=jmeter-master -o jsonpath='{.item
 # Setup test cases
 misc_setup() {
   export MASTER_NAME=$(kubectl get pods -l wdias=jmeter-master -o jsonpath='{.items[*].metadata.name}')
-  kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter setup 1440" && sleep 3 && echo "\n\n" \
+  kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter setup 1440"
+  sleep 3 && echo -e "\n\n"
   kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter create_timeseries 1440"
-  echo "Setup the MISC test. Exit"
+  echo "Setup the MISC test complete. Exit"
   exit 0
 }
 
@@ -23,7 +24,7 @@ fi
 misc_run() {
   TEST_CASE=$1
   REQ_SIZE=$2
-  echo "\n\nRunning test: ${TEST_CASE} for reqSize: ${REQ_SIZE}"
+  echo -e "\n\nRunning test: ${TEST_CASE} for reqSize: ${REQ_SIZE}"
   kubectl exec -it $MASTER_NAME -- bash -c "./test-plan/test_plan.sh /jmeter ${TEST_CASE} ${REQ_SIZE}"
   echo "Copy jmeter output wdias_${TEST_CASE}.jtl >> wdias_${TEST_CASE}_${REQ_SIZE}.jtl"
   kubectl get pods | grep 'wdias-performance-test-master' | awk '{print $1}' | xargs -o -I {} kubectl cp default/{}:/jmeter/logs/wdias_${TEST_CASE}.jtl ./logs/wdias_${TEST_CASE}_${REQ_SIZE}.jtl
