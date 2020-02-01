fb7141a57926c116cb6b25bbd41fe080d89db9c3
gihankarunarathne <gckarunarathne@gmail.com>
2020-02-01 22:51:09 +0400

    Moved export after the import

--- test-plan/misc_test.sh
+++ test-plan/misc_test.sh
@@ -72,10 +72,10 @@ misc_all() {
 
 misc_other() {
   misc_cleanup import pod && misc_run import 1440
-  misc_cleanup create_extensions pod && misc_run create_extensions 1440
-  misc_cleanup extension && misc_run extension 1440
   misc_cleanup "export" && misc_run "export" 1440
   misc_cleanup query && misc_run query 1440
+  misc_cleanup create_extensions pod && misc_run create_extensions 1440
+  misc_cleanup extension && misc_run extension 1440
 }
 
 misc_help() {
